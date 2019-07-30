05a2499ce96706b0ca551bfca5e576a34c1472bc
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-07-30 14:02:37 +0300

    add edit ussd

--- server.js
+++ server.js
@@ -13,35 +13,88 @@ app.get('*', (req, res) => {
 })
 
 app.post('*', (req, res) => {
-  let {sessionId, serviceCode, phoneNumber, text} = req.body
+  let {sessionId, serviceCode, phoneNumber, text} = req.body;
+  let length = text.split("*").length;
+  let txt =text.split("*");
   if (text == '') {
     // This is the first request. Note how we start the response with CON
     let response = `CON What would you want to check
-    1. My Account
-    2. My phone number`
+    1. Search Rider
+    2. Rate Rider`
     res.send(response)
   } else if (text == '1') {
     // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`
+    let response = `CON Please enter the motorbike plate number:`
     res.send(response)
   } else if (text == '2') {
     // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
+    let response = `CON Please enter the motorbike plate number:`
     res.send(response)
-  } else if (text == '1*1') {
-    // Business logic for first level response
-    let accountNumber = 'ACC1001'
+  } else if (length === 2) {
+    let initial_selection = txt[0];
+    if(initial_selection == '1'){
+      // search rider
+      // query from databse
+      // let sms_message = `We are not able to verify the rider information provided.`;
+       let rider_detail = txt[length - 1];
+      if(rider_detail==="KMEE744N"){
+      //   let rider_name = rider_detail.name;
+        sms_message = `Rider Obwollo (KMEE744N) is registered with Makoma Sacco.`;
+    } else {sms_message = `We are not able to verify the rider information provided.`}
+        const credentials = {
+          apiKey: '546c73eecdc1ab4ba9815fb43bdcd5129b4ce1b3a94ac9cdead025bfebef68a2',
+          username: 'nyatindopatrick',
+      }
+      
+      // Initialize the SDK
+      const AfricasTalking = require('africastalking')(credentials);
+      
+      // Get the SMS service
+      const sms = AfricasTalking.SMS;
+      
+      function sendMessage() {
+          const options = {
+              // Set the numbers you want to send to in international format
+              to: [client_phone_number],
+              // Set your message
+              message: sms_message
+              // Set your shortCode or senderId
+          }
+      
+          // That’s it, hit send and we’ll take care of the rest
+          sms.send(options)
+              .then(console.log)
+              .catch(console.log);
+      }
+      
+      sendMessage();
+      };
+      
+      // initialize Africas Talking
+      // send SMS
+      // sendSMS(client_phone_number, sms_message);
+      // send sms back to customer
+      let response='END Thank you for your query. You will receive an SMS shortly';
+      res.send(response);
+    }
+    else if(initial_selection == '2'){
+      // rate rider
+      // Business logic for first level response
+      let response= 'CON Rate a rider on a scale of 1 to 5';
+      // This is a terminal request. Note how we start the response with END
+      
+      res.send(response);
+    }
+  
+  else if(length === 3){
+    // let rider_rating = txt[length - 1];
+    // let plate_number = txt[length - 2];
+    // go to databse get rider information
+    // set rating for rider and upate
+    let response= 'END Thank you for your feedback.';
     // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`
-    res.send(response)
-  } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000'
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`
-    res.send(response)
+    
+    res.send(response);
   } else {
     res.status(400).send('Bad request!')
   }
