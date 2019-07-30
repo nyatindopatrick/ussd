4fd985908b7d8aa86c6520cc322f9942098edfe3
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-07-30 13:46:58 +0300

    add edit ussd

--- server.js
+++ server.js
@@ -1,107 +1,52 @@
-const express = require('express');
-const app = express();
-const bodyParser = require ('body-parser');
-const logger = require('morgan');
-const port = process.env.PORT || 3030;
+const app = require('express')()
+const bodyParser = require('body-parser')
+const logger = require('morgan')
 
+const port = process.env.PORT || 3030
 
-app.get('*',(req,res)=>{
-  res.send("USSD Code")
-});
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('*',(req,res) => {
-  let { sessionId, phoneNumber, serviceCode, text} = req.body;
-  let length = text.split("*").length;
-  let txt =text.split("*");
+app.get('*', (req, res) => {
+  res.send('This is tutorial App on creating your first USSD app in 5 minutes or less by Ajala Abdulsamii <kgasta@gmail.com>')
+})
+
+app.post('*', (req, res) => {
+  let {sessionId, serviceCode, phoneNumber, text} = req.body
   if (text == '') {
     // This is the first request. Note how we start the response with CON
-    let response = `CON Welcome to Fika Safe. Please pick an option below
-    1. Search a rider
-    2. Rate a rider
-    `
+    let response = `CON What would you want to check
+    1. My Account
+    2. My phone number`
     res.send(response)
   } else if (text == '1') {
     // Business logic for first level response
-    let response = `CON Please enter the motorbike plate number`
+    let response = `CON Choose account information you want to view
+    1. Account number
+    2. Account balance`
     res.send(response)
   } else if (text == '2') {
     // Business logic for first level response
-    let response = `CON Enter the motorbike plate number:`
+    let response = `END Your phone number is ${phoneNumber}`
     res.send(response)
-  }
-  else if(length === 2){
-    let initial_selection = txt[0];
-    // let phone_number = txt[length - 1];
-     let client_phone_number = phoneNumber;
-     let sms_message ;
-    if(initial_selection == '1'){
-      // search rider
-      // query from databse
-      // let sms_message = `We are not able to verify the rider information provided.`;
-       let rider_detail = txt[length - 1];
-      if(rider_detail==="KMEE744N"){
-   
-        sms_message = `Rider Obwollo (KMEE744N) is registered with Makoma Sacco.`;
-    } else {sms_message = `We are not able to verify the rider information provided.`}
-        const credentials = {
-          apiKey: '546c73eecdc1ab4ba9815fb43bdcd5129b4ce1b3a94ac9cdead025bfebef68a2',
-          username: 'nyatindopatrick',
-      }
-      
-      // Initialize the SDK
-      const AfricasTalking = require('africastalking')(credentials);
-      
-      // Get the SMS service
-      const sms = AfricasTalking.SMS;
-      
-      function sendMessage() {
-          const options = {
-              // Set the numbers you want to send to in international format
-              to: [client_phone_number],
-              // Set your message
-              message: sms_message
-              // Set your shortCode or senderId
-          }
-      
-          // That’s it, hit send and we’ll take care of the rest
-          sms.send(options)
-              .then(console.log)
-              .catch(console.log);
-      }
-      
-      sendMessage();
-      };
-      
-      // initialize Africas Talking
-      // send SMS
-      // sendSMS(client_phone_number, sms_message);
-      // send sms back to customer
-      let response='END Thank you for your query. You will receive an SMS shortly';
-      res.send(response);
-    }
-    else if(initial_selection == '2'){
-      // rate rider
-      // Business logic for first level response
-      let response= 'CON Rate a rider on a scale of 1 to 5';
-      // This is a terminal request. Note how we start the response with END
-      
-      res.send(response);
-    }
-  
-  else if(length === 3){
-    // let rider_rating = txt[length - 1];
-    // let plate_number = txt[length - 2];
-    // go to databse get rider information
-    // set rating for rider and upate
-    let response= 'END Thank you for your feedback.';
+  } else if (text == '1*1') {
+    // Business logic for first level response
+    let accountNumber = 'ACC1001'
     // This is a terminal request. Note how we start the response with END
-    
-    res.send(response);
+    let response = `END Your account number is ${accountNumber}`
+    res.send(response)
+  } else if (text == '1*2') {
+    // This is a second level response where the user selected 1 in the first instance
+    let balance = 'NGN 10,000'
+    // This is a terminal request. Note how we start the response with END
+    let response = `END Your balance is ${balance}`
+    res.send(response)
+  } else {
+    res.status(400).send('Bad request!')
   }
-    else {
-    res.status(400).send('Bad request!');
-  }
-});
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
-});
+})
